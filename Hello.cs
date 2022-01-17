3b13cabf18b442134ae5d31ff415bcfe12a7d4c5
MujabasicMirnes <97909711+MujabasicMirnes@users.noreply.github.com>
2022-01-17 19:45:09 +0100

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -3,12 +3,7 @@ public class Hello
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-      System.Console.WriteLine("Hello, Local!");
-    }
-}
-=======
       System.Console.WriteLine("Hello, GitHub!");
    }
 }
->>>>>>> ff1982e3e5b7eb47b96c1a22304a105ad4653e88
+
